67ed1aa15e20fb4550f3f12508c9ad6081744231
dineshc810 <63349010+dineshc810@users.noreply.github.com>
2020-07-27 17:43:08 +0530

    Add files via upload

--- python_oops.py
+++ python_oops.py
@@ -4,190 +4,148 @@ Created on Wed Jul 22 18:21:07 2020
 
 @author: Dinesh.Choudhary
 """
-
-class gun:
+        
+class Player:
     def __init__(self):
-        self.name='GUN:'
+        self.type=self.__class__.__name__
 
-class pistol(gun):
+
+class Terrorist(Player):             
     def __init__(self):
-        self.name='PISTOL-'
-    def printguntype(self):
-        super().__init__()
-        G1=self.name  + 'PISTOL-'
-        print(G1)   
+        self.type=f'{Player.__name__}:{self.__class__.__name__}'
+        
+class Counter_Terrorist(Player):             
+    def __init__(self):
+        self.type=f'{Player.__name__}:{self.__class__.__name__}'
+
+
+        
+class T1(Terrorist):             
+    def __init__(self):
+        self.type=f'{Player. __name__}:{Terrorist.__name__}:{self.__class__.__name__}'
+        
+class T2(Terrorist):             
+    def __init__(self):
+        self.type=f'{Player. __name__}:{Terrorist.__name__}:{self.__class__.__name__}'
+        
+class CT1(Counter_Terrorist):             
+    def __init__(self):
+        self.type=f'{Player. __name__}:{Counter_Terrorist.__name__}:{self.__class__.__name__}'
+        
+class CT2(Counter_Terrorist):             
+    def __init__(self):
+        self.type=f'{Player. __name__}:{Counter_Terrorist.__name__}:{self.__class__.__name__}'
     
 
-class rifle(gun):
+
+
+class Gun:
     def __init__(self):
-        self.name='RIFLE-'
-    def printguntype(self):
-        super().__init__()
-        G2=self.name  + 'RIFLE-'
-        print(G2) 
+        self.type=self.__class__.__name__
+     
         
-
-class flare_gun(pistol):
+class Pistol(Gun):
     def __init__(self):
-        self.name='FLARE GUN' 
-    def printgunsubtype(self):
-        super().printguntype()
-        G3= Pistol.G1  + 'FLARE GUN-'
-        print(G3) 
+        self.type=f'{Gun.__name__}:{self.__class__.__name__}'
 
-class deagle(pistol):
+class Rifle(Gun):
     def __init__(self):
-        self.name='DEAGLE'            
-        
+       self.type=f'{Gun.__name__}:{self.__class__.__name__}'
 
-class assault_rifle(rifle):
+
+
+class Flare_Gun(Pistol):
     def __init__(self):
-        self.name='ASSAULT RIFLE-'
+        self.type=f'{Gun. __name__}:{Pistol.__name__}:{self.__class__.__name__}'
+    
+class Deagle(Pistol):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Pistol.__name__}:{self.__class__.__name__}'      
+
+class Skorpion(Pistol):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Pistol.__name__}:{self.__class__.__name__}'      
+
+class Assault_Rifle(Rifle):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{self.__class__.__name__}'
  
-class sniper(rifle):
+class Sniper(Rifle):
     def __init__(self):
-        self.name='SNIPER-'
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{self.__class__.__name__}'
         
+class Shotgun(Rifle):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{self.__class__.__name__}'
+
+class Machinegun(Rifle):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{self.__class__.__name__}'        
         
+class Groza(Assault_Rifle):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Assault_Rifle.__name__}:{self.__class__.__name__}'
         
-Gun=gun()
-Pistol=pistol()
-Rifle=rifle()
-Flare_Gun=flare_gun()
-Deagle=deagle()
-Assault_Rifle=assault_rifle()
-Sniper=sniper()
+class AK47(Assault_Rifle):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Assault_Rifle.__name__}:{self.__class__.__name__}'
+        
+class AWM(Sniper):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Sniper.__name__}:{self.__class__.__name__}'
 
-
-print("Enter the gun: (Pistol)(Rifle)")
-Gun=input()
-
-print("Enter the gun type: (Flare gun)(Deagle)(Skorpion)(Assault Rifle)(Sniper)(Shotgun)(Machine gun)")
-Gun_type=input()
-
-
-if((Gun=='Pistol') ):
-    if(Gun_type=='Flare gun'):
-        Flare_Gun.printgunsubtype()
+class M24(Sniper):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Sniper.__name__}:{self.__class__.__name__}'   
+        
+class DBS(Shotgun):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Shotgun.__name__}:{self.__class__.__name__}'
+  
+class S686(Shotgun):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Shotgun.__name__}:{self.__class__.__name__}'  
+        
+class MG42(Machinegun):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Machinegun.__name__}:{self.__class__.__name__}' 
+  
+class ThompsonMG(Machinegun):
+    def __init__(self):
+        self.type=f'{Gun. __name__}:{Rifle.__name__}:{Machinegun.__name__}:{self.__class__.__name__}'   
         
 
-         
-        
-elif ((Gun=='Rifle') ) :    
-    Rifle.printguntype()
+
+player=Player()
+terrorist=Terrorist()
+counter_terrorist=Counter_Terrorist()   
+t1=T1()
+t2=T2()
+ct1=CT1()
+ct2=CT2()
 
 
+gun=Gun()
 
+pistol=Pistol()
+rifle=Rifle()
 
+flare_gun=Flare_Gun()
+deagle=Deagle()
+skorpion=Skorpion()
+assault_rifle=Assault_Rifle()
+sniper=Sniper()
+shotgun=Shotgun()
+machinegun=Machinegun()
 
-
-
-                        
-#class flare_gun(pistol):
-#    def __init__(self):
-#        self.name='FLARE GUN'        
-#        
-#class skorpion(pistol):
-#    def __init__(self):
-#        self.name='SKORPION'
-#        
-#class deagle(pistol):
-#    def __init__(self):
-#        self.name='DEAGLE'       
-#        
-#
-#
-#class assault_rifle(rifle):
-#    def __init__(self):
-#        self.name='ASSAULT RIFLE-'
-# 
-#class sniper(rifle):
-#    def __init__(self):
-#        self.name='SNIPER-'
-#        
-#class shotgun(rifle):
-#    def __init__(self):
-#        self.name='SHOTGUN-'
-#
-#class machinegun(rifle):
-#    def __init__(self):
-#        self.name='MACHINEGUN-'
-#        
-#class groza(assault_rifle):
-#    def __init__(self):
-#        self.name='GROZA-'
-#        
-#class AK47(assault_rifle):
-#    def __init__(self):
-#        self.name='AK47-'
-#        
-#class AWM(sniper):
-#    def __init__(self):
-#        self.name='AWM-'
-#
-#class M24(sniper):
-#    def __init__(self):
-#        self.name='M24-'   
-#        
-#class DBS(shotgun):
-#    def __init__(self):
-#        self.name='DBS-'   
-#  
-#class S686(shotgun):
-#    def __init__(self):
-#        self.name='S686-'   
-#        
-#class MG42(machinegun):
-#    def __init__(self):
-#        self.name='MG42-'   
-#  
-#class ThompsonMG(machinegun):
-#    def __init__(self):
-#        self.name='THOMPSONMG-'   
-#        
-#        
-#class Player:
-#    def __init__(self):
-#        self.name='Player-'
-#        print(self.name)
-#
-#class Terrorist(Player):             
-#    def __init__(self):
-#        self.name='Terrorist-'
-#        
-#class T1(Terrorist):             
-#    def __init__(self):
-#        self.name='T1-'
-#        
-#class T2(Terrorist):             
-#    def __init__(self):
-#        self.name='T2-'
-#        
-#class Counter_Terrorist(Player):             
-#    def __init__(self):
-#        self.name='Counter_Terrorist-'   
-#        print(self.name)
-#    
-#class CT1(Counter_Terrorist):             
-#    def __init__(self):
-#        self.name='CT1-'
-#        print(self.name)
-#
-#class CT2(Counter_Terrorist):             
-#    def __init__(self):
-#        self.name='CT2-'    
-#    
-#P=Player()
-#T=Terrorist()
-#CT=Counter_Terrorist()   
-#T1=T1()
-#T2=T2()
-#CT1=CT1()
-#CT2=CT2()
-#Gun=gun()
-#Pistol=pistol()
-#Rifle=rifle()
-
+groza=Groza()
+ak47=AK47()
+awm=AWM()
+m24=M24()
+dbs=DBS()
+s686=S686()
+mg42=MG42()
+thompsonmg=ThompsonMG()
 
 
 
@@ -195,22 +153,111 @@ elif ((Gun=='Rifle') ) :
 
 #main
 
-#print("Enter the player name: (Terrorist)(Counter-Terrorist)")
-#Player_name=input()
-#
-#print("Enter the player type: (T1)(T2)(CT1)(CT2)")
-#Player_type=input()
+print("Enter the player: (Terrorist)(Counter Terrorist)")
+player_input=input()
+
+print("Enter the player type: (T1)(T2)(CT1)(CT2)")
+player_type_input=input()
+
+print("Enter the gun: (Pistol)(Rifle)")
+gun_input=input()
+
+print("Enter the gun type: (Flare gun)(Deagle)(Skorpion)(Assault Rifle)(Sniper)(Shotgun)(Machine gun)")
+gun_type_input=input()
+
+print("Enter the gun sub type: (Groza)(AK47)(M24)(AWM)(DBS)(S686)((MG42)(Thompson MG)")
+gun_subtype_input=input()
 
 
-#print("Enter the gun sub type: (Groza)(AK47)(M24)(AWM)(DBS)(S686)((MG42)(Thompson MG)")
-#Gun_subtype=input()
-#
 
-#if((Player_name=='Counter-Terrorist') & (Player_type=='CT1')):
-#    print(CT1.name)
-#else: 
-#    print('scheme')
-#    
+if(player_input=='Terrorist'):
+    if (player_type_input=='T1'):
+        print(t1.type)
+        
+        if(gun_input=='Pistol'):
+            if (gun_type_input=='Flare Gun'):
+                print(flare_gun.type)
+            elif (gun_type_input=='Deagle'):
+                print(deagle.type) 
+            elif (gun_type_input=='Skorpion'):
+                print(skorpion.type) 
+        elif (gun_input=='Rifle'):   
+            if (gun_type_input=='Assault Rifle'):
+                print(assault_rifle.type)
+            if (gun_type_input=='Sniper'):
+                print(sniper.type) 
+            if (gun_type_input=='Shotgun'):
+                print(shotgun.type)
+            if (gun_type_input=='Machinegun'):
+                print(machinegun.type)         
+        
+    elif (player_type_input=='T2'):
+        print(t2.type) 
+        
+        if(gun_input=='Pistol'):
+            if (gun_type_input=='Flare Gun'):
+                print(flare_gun.type)
+            elif (gun_type_input=='Deagle'):
+                print(deagle.type) 
+            elif (gun_type_input=='Skorpion'):
+                print(skorpion.type) 
+        elif (gun_input=='Rifle'):   
+            if (gun_type_input=='Assault Rifle'):
+                print(assault_rifle.type)
+            if (gun_type_input=='Sniper'):
+                print(sniper.type) 
+            if (gun_type_input=='Shotgun'):
+                print(shotgun.type)
+            if (gun_type_input=='Machinegun'):
+                print(machinegun.type)         
+        
+elif(player_input=='Counter Terrorist'):
+    if (player_type_input=='CT1'):
+        print(ct1.type)
+        
+        if(gun_input=='Pistol'):
+            if (gun_type_input=='Flare Gun'):
+                print(flare_gun.type)
+            elif (gun_type_input=='Deagle'):
+                print(deagle.type) 
+            elif (gun_type_input=='Skorpion'):
+                print(skorpion.type) 
+        elif (gun_input=='Rifle'):   
+            if (gun_type_input=='Assault Rifle'):
+                print(assault_rifle.type)
+            if (gun_type_input=='Sniper'):
+                print(sniper.type) 
+            if (gun_type_input=='Shotgun'):
+                print(shotgun.type)
+            if (gun_type_input=='Machinegun'):
+                print(machinegun.type)         
+        
+    elif (player_type_input=='CT2'):
+        print(ct2.type) 
+        
+        if(gun_input=='Pistol'):
+            if (gun_type_input=='Flare Gun'):
+                print(flare_gun.type)
+            elif (gun_type_input=='Deagle'):
+                print(deagle.type) 
+            elif (gun_type_input=='Skorpion'):
+                print(skorpion.type) 
+        elif (gun_input=='Rifle'):   
+            if (gun_type_input=='Assault Rifle'):
+                print(assault_rifle.type)
+            if (gun_type_input=='Sniper'):
+                print(sniper.type) 
+            if (gun_type_input=='Shotgun'):
+                print(shotgun.type)
+            if (gun_type_input=='Machinegun'):
+                print(machinegun.type)         
+
+
+
+
+
+
+
 
 
 
